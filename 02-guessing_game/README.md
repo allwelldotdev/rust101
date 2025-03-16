de3044f4fe8bbf033adbee486ae6a5c92ab30693
Allwell Agwu-Okoro <47219269+allwelldotdev@users.noreply.github.com>
2025-03-16 12:20:25 +0100

    build guessing game

---
+++ 02-guessing_game/README.md
@@ -0,0 +1,5 @@
+# Programming a Guessing Game in Rust
+
+Following the instructions from The Rust book, I built a guessing game in Rust. A nice way to dive head-first in learning Rust programming.
+
+I learned new Rust concepts like `let`, `match`, functions, the use of external crates, and more.
---
+++ 02-guessing_game/src/main.rs
@@ -0,0 +1,44 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    // generate secret random number
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    
+    // println!("The secret number is {secret_number}");
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        // take user input: guess number
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // convert `guess` from String type to u32 - this uses a concept in Rust known as Shadowing
+        // let guess: u32 = guess.trim().parse().expect("Please type a number");
+
+        // instead of crashing program when `guess` is non-number, continue
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+            
+        println!("You guessed: {guess}");
+
+        // compare guess value with secret random number
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
+}
