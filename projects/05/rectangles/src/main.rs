4d59fcc56bf5ef18460ba70e2464b58868b55a6f
Allwell Agwu-Okoro <47219269+allwelldotdev@users.noreply.github.com>
2025-08-05 21:45:29 +0100

    project: using structs: refactoring with tuples

--- projects/05/rectangles/src/main.rs
+++ projects/05/rectangles/src/main.rs
@@ -1,16 +1,31 @@
 // An example program using structs
 // Take the width and height of a rectangle specified in pixels and calculate the area of the rectangle.
 
+// fn main() {
+//     let width1 = 30;
+//     let height1 = 50;
+
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(width1, height1)
+//     );
+// }
+
+// fn area(width: u32, height: u32) -> u32 {
+//     width * height
+// }
+
+// Refactoring with Tuples
+
 fn main() {
-    let width1 = 30;
-    let height1 = 50;
+    let rect1 = (30, 50);
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(width1, height1)
+        area(rect1)
     );
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(dimensions: (i32, i32)) -> i32 {
+    dimensions.0 * dimensions.1
 }
