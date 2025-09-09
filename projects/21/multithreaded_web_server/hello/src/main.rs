823cf4114deb75c09b9c436e288bc222a4a4630d
Allwell Agwu-Okoro <47219269+allwelldotdev@users.noreply.github.com>
2025-09-09 15:35:13 +0100

    project: final project: single-threaded web server: send 404 page for unresolved requests

--- projects/21/multithreaded_web_server/hello/src/main.rs
+++ projects/21/multithreaded_web_server/hello/src/main.rs
@@ -1,8 +1,12 @@
 // Listen to TCP Connection
+// Building a web server - Single-threaded
+#[allow(unused)]
 use std::{
     fs,
     io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 fn main() {
@@ -10,9 +14,7 @@ fn main() {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
         // println!("Connection established");
-
         handle_connection(stream);
     }
 }
@@ -20,21 +22,64 @@ fn main() {
 #[allow(unused)]
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader
+
+    // let http_request: Vec<_> = buf_reader
+    //     .lines()
+    //     .map(|result| result.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect();
+
+    // println!("Request: {http_request:#?}");
+
+    // Validating requests, responding selectively
+    let request_line = buf_reader
         .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+        .next()
+        // There's a bug here: when the browser doesn't make a fresh request to the
+        // server, the iterator is empty, `next()` returns `None`. `expect()` fires error message:
+        // "No first line request"
+        .expect("No first line request")
+        .expect("Could not read HTTP buffer (first line)");
 
-    println!("Request: {http_request:#?}");
+    // // if http_request[0] == "GET / HTTP/1.1" {
+    // if request_line == "GET / HTTP/1.1" {
+    //     let status_line = "HTTP/1.1 200 OK";
+    //     let contents =
+    //         fs::read_to_string("hello.html").expect("Could not parse \"hello.html\" file");
+    //     let length = contents.len();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").expect("Could not parse html file");
-    let length = contents.len();
+    //     // The reason for the double CRLF `\r\n` is one new line after header,
+    //     // then second new line to usher in message-body.
+    //     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    //     stream
+    //         .write_all(response.as_bytes())
+    //         .expect("Could not write response to send buffer");
+    // } else {
+    //     let status_line = "HTTP/1.1 400 NOT FOUND";
+    //     let contents = fs::read_to_string("404.html").expect("Could not parse \"404.html\" file");
+    //     let length = contents.len();
 
-    // The reason for the double CRLF `\r\n` is one new line after header,
-    // then second new line to usher in message body.
-    let response = format!("{status_line}\r\nContext-Length: {length}\r\n\r\n{contents}");
+    //     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    //     stream
+    //         .write_all(response.as_bytes())
+    //         .expect("Could not write response to send buffer");
+    // }
+
+    // --------------
+
+    // Refactoring repetitive code
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 400 NOT FOUND", "404.html")
+    };
+
+    let contents =
+        fs::read_to_string(filename).expect(&format!("Could not parse \"{filename}\" file"));
+    let response = format!(
+        "{status_line}\r\nContent-Length: {}\r\n\r\n{contents}",
+        contents.len()
+    );
     stream
         .write_all(response.as_bytes())
         .expect("Could not write response to send buffer");
